fce319e8820f045f3fd049518a5f295be188f1d3
Bharadwaja <bharadwajach@hotmail.com>
2018-04-14 14:35:11 -0500

    changed the message by BC

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Bharadwaja Cheruvu\n");
+  response.end("Hello Bharadwaja Cheruvu - K8 BEST Example\n");
 });
 
 // listen on localhost:8000
